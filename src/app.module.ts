0847ef39d8f7a72112362b92d3cc5b0801ebb3de
hakeemnazri <mhakeemnazri@gmail.com>
2025-10-18 13:33:28 +0800

    Feat: prisma & docker setrup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
+import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './domain/users/users.module';
 
 @Module({
-  imports: [UsersModule, CommonModule],
+  imports: [UsersModule, CommonModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
